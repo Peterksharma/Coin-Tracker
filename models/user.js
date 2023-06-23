c1c5bbc751c7cd2e6e8526bf0168b96655016ed2
Peterksharma <120028593+Peterksharma@users.noreply.github.com>
2023-06-22 22:16:07 -0500

    server updated with pasport middleware

---
+++ models/user.js
@@ -0,0 +1,8 @@
+
+
+//Primary key
+
+
+bcrypt.hash(password, 10, function(err, hash) {
+    // Store hashed password in your User model
+  });
--- server.js
+++ server.js
@@ -1,18 +1,40 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
 const sequelize = require('./config/connection');
-
+const passport = require('passport');
+require('./config/passport')(passport);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//Express Session Middleware
+//need to replace secret with a secret key
+app.use(session({ secret: 'secret', resave: true, saveUninitialized: true }));
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.findByPk(id).then((user) => {
+        done(null, user);
+    }).catch(done);
+});
+
+//connects the login route with the authenticate
+app.post('/login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login' }));
+
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
 
+// sync sequelize models to the database, then turn on the server
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`App is listen of the Port ${PORT}`));
