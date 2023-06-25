e3275e1a915561d9ba4de9f89694663618d4b09c
Peterksharma <120028593+Peterksharma@users.noreply.github.com>
2023-06-24 19:05:06 -0500

    seed data up to 50 state quarters

--- seeds/coinSeedData.js
+++ seeds/coinSeedData.js
@@ -59,3 +59,47 @@ for (let year = 1938; year <= 2023; year++) {
 console.log(jeffNickles);
 
 //Dimes
+
+let mercDimes = [];
+
+for (let year = 1916; year <= 1945; year++) {
+    for (let mark of mintMarks) {
+        mercDimes.push({
+            name: 'Mercury Dime',
+            denomination: 0.10,
+            year: year,
+            mintMark: mark
+        });
+    }
+}
+console.log(mercDimes);
+
+let rooseveltDimes = [];
+
+for (let year = 1946; year <= 2023; year++) {
+    for (let mark of mintMarks) {
+        rooseveltDimes.push({
+            name: 'Roosevelt Dime',
+            denomination: 0.10,
+            year: year,
+            mintMark: mark
+        });
+    }
+}
+console.log(rooseveltDimes);
+
+
+//Quarters
+let washingtonQuarter = [];
+
+for (let year = 1932; year <= 1998; year++) {
+    for (let mark of mintMarks) {
+        washingtonQuarter.push({
+            name: 'Washington Quarter',
+            denomination: 0.25,
+            year: year,
+            mintMark: mark
+        });
+    }
+}
+console.log(washingtonQuarter);
