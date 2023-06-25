07bfc18a32f3e742060685b52ec9864e35419ba0
Fernando Siller <fernando@Fernandos-MBP.attlocal.net>
2023-06-24 20:15:45 -0500

    user table

--- models/user.js
+++ models/user.js
@@ -1,4 +1,33 @@
+// const sequelize = require('sequelize');
 
+// DROP DATABASE IF EXISTS coins
+
+// const sequelize = new Sequelize //(databasename, username, password, {
+//   //host: database host,
+//   //dialect: database driver,
+// //});
+
+// const {DataTypes} = require ('sequelize');
+
+// const User = sequelize.define(User, {
+//   id: {
+//     type: DataTypes.INTEGER,
+//     primaryKey: true,
+//     autoIncrement: true,
+//   },
+
+//   userName: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+// })
+
+const {Model, DataTypes} = require ('sequelize');
+const sequelize = require('../config/connection.js')
+
+class users extends Model {}
+
+users.init 
 
 //Primary key
 
