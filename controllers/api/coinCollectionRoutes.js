1f9e660c39fe20fb6bb2e982cee1f88aaf4b8fdc
Peterksharma <120028593+Peterksharma@users.noreply.github.com>
2023-07-05 21:27:09 -0500

    got the routes to add the coin to user as well as the mint mark connected

--- controllers/api/coinCollectionRoutes.js
+++ controllers/api/coinCollectionRoutes.js
@@ -1,55 +1,58 @@
 const router = require('express').Router();
 const { Coin } = require('../../models');
 
-// route to get all the coins
-router.get('/', (req, res) => {
-    Coin.findAll({
-        include: {
-            atrributes: ['name', 'denomination', 'year', 'mintMark']
 
-        }
-    })
-        .then(coinData => {
-            if (!coinData) {
-                res.status(404).json({ message: 'Thank you, but you are poor and there are no coins' })
-            }
-            res.json(coinData);
-        })
-        .catch(err => {
-            console.log(err);
-            res.json(500).json(err)
-        })
-})
+// <--------------SAVE THIS----------->
+// // route to get all the coins
+// router.get('/', (req, res) => {
+//     Coin.findAll({
+//         include: {
+//             atrributes: ['name', 'denomination', 'year', 'mint']
 
-//get a specific coin
-router.get('/:id', (req, res) => {
-    coinCollection.findOne({
-        where: {
-            id: req.params.id
-        },
-        atrributes: ['name', 'denomination', 'year', 'mintMark']
+//         }
+//     })
+//         .then(coinData => {
+//             if (!coinData) {
+//                 res.status(404).json({ message: 'Thank you, but you are poor and there are no coins' })
+//             }
+//             res.json(coinData);
+//         })
+//         .catch(err => {
+//             console.log(err);
+//             res.json(500).json(err)
+//         })
+// })
 
-    })
-    .then(coinData => {
-        if (!coinData) {
-            res.status(404).json({ message: 'No coin found with this id' });
-            return;
-        }
-        res.json(coinData);
-    })
-        .catch(err => {
-            console.log(err);
-            res.json(500).json(err)
-        })
-})
+// //get a specific coin
+// router.get('/:id', (req, res) => {
+//     coinCollection.findOne({
+//         where: {
+//             id: req.params.id
+//         },
+//         atrributes: ['name', 'denomination', 'year', 'mint']
+
+//     })
+//     .then(coinData => {
+//         if (!coinData) {
+//             res.status(404).json({ message: 'No coin found with this id' });
+//             return;
+//         }
+//         res.json(coinData);
+//     })
+//         .catch(err => {
+//             console.log(err);
+//             res.json(500).json(err)
+//         })
+// })
 
 //This route will be for adding a new coin to the collectio
 router.post('/', (req, res) => {
-    Coin.create({
+    Coin.create({   
+        user_id: req.user.id,
         name: req.body.name,
         denomination: req.body.denomination,
         year: req.body.year,
-        mintMark: req.body.mint
+        mint: req.body.mint
     })
     .then(coinData =>  res.json({
         message: `Coin has beed added to the database`,
